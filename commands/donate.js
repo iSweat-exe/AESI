e739d9d87be9467491542aa4931731f0762ccb64
isweat-exe <platiniumsamu@gmail.com>
2023-11-26 22:34:54 +0100

    New command : donate.js

--- commands/donate.js
+++ commands/donate.js
@@ -4,68 +4,66 @@ const profileModel = require("../models/profileSchema");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("donate")
-    .setDescription("Donate coins to another user.")
-    .addUserOption(option =>
-      option.setName("target")
-        .setDescription("The user you want to donate to.")
+    .setDescription("Donate your coins to another user")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("The user you want to donate to")
         .setRequired(true)
     )
-    .addIntegerOption(option =>
-      option.setName("amount")
-        .setDescription("The amount of coins you want to donate.")
+    .addIntegerOption((option) =>
+      option
+        .setName("amount")
+        .setDescription("The amount of coins you want to donate")
         .setRequired(true)
+        .setMinValue(1)
     ),
   async execute(interaction, profileData) {
-    const { id } = interaction.user;
-    const targetUser = interaction.options.getUser("target");
-    const amount = interaction.options.getInteger("amount");
+    const receiveUser = interaction.options.getUser("user");
+    const donateAmt = interaction.options.getInteger("amount");
 
-    // Check if the donation amount is non-negative
-    if (amount <= 0) {
-      await interaction.reply("Please enter a positive amount to donate.");
-      return;
-    }
+    const { balance } = profileData;
 
-    // Check if the user has enough coins to donate
-    if (profileData.balance < amount) {
-      await interaction.reply("You don't have enough coins to donate.");
-      return;
-    }
-
-    try {
-      // Deduct the donated amount from the donor's balance
-      await profileModel.findOneAndUpdate(
-        { userId: id },
-        {
-          $inc: {
-            balance: -amount,
-          },
-        }
+    if (balance < donateAmt) {
+      await interaction.deferReply({ ephemeral: true });
+      return await interaction.editReply(
+        `You do not have ${donateAmt} coins in your balance`
       );
+    }
 
-      // Find the profile of the target user
-      const targetProfile = await profileModel.findOne({ userId: targetUser.id });
-
-      // If the target user doesn't exist, you may handle it as needed
-      if (!targetProfile) {
-        await interaction.reply("The specified user does not have a profile.");
-        return;
+    const receiveUserData = await profileModel.findOneAndUpdate(
+      {
+        userId: receiveUser.id,
+      },
+      {
+        $inc: {
+          balance: donateAmt,
+        },
       }
+    );
 
-      // Add the donated amount to the target user's balance
-      await profileModel.findOneAndUpdate(
-        { userId: targetUser.id },
-        {
-          $inc: {
-            balance: amount,
-          },
-        }
+    if (!receiveUserData) {
+      await interaction.deferReply({ ephemeral: true });
+      return await interaction.editReply(
+        `${receiveUser.username} is not in the currency system.`
       );
-
-      await interaction.reply(`You have successfully donated ${amount} coins to ${targetUser.username}.`);
-    } catch (err) {
-      console.error(err);
-      await interaction.reply("An error occurred while processing your donation.");
     }
+
+    await interaction.deferReply();
+
+    await profileModel.findOneAndUpdate(
+      {
+        userId: interaction.user.id,
+      },
+      {
+        $inc: {
+          balance: -donateAmt,
+        },
+      }
+    );
+
+    interaction.editReply(
+      `You have donated ${donateAmt} coins to ${receiveUser.username}`
+    )
   },
 };
